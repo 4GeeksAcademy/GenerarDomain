60edb0e1b07e9510eec25cd2dc7f999a6d16df32
JavierML-git <mjavivi673@gmail.com>
2025-02-03 19:46:18 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".es", ".com", ".net"];
+
+  function domgenerator(pronoun, adj, noun, extension) {
+    let newar = [];
+
+    for (let j of adj) {
+      for (let i of pronoun) {
+        for (let k of noun) {
+          for (let l of extension) {
+            newar.push(`${i}${j}${k}${l}`);
+          }
+        }
+      }
+    }
+    return newar;
+  }
+
+  let solucion = domgenerator(pronoun, adj, noun, extension);
+
+  document.getElementById("generador").innerText = solucion.join("\n");
 };
